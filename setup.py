c5c5a7e2b1c030f777613d9492c3fc35b6db59db
Xero64 <37905073+Xero64@users.noreply.github.com>
2024-07-26 17:38:49 +0200

    Version bump and Python version bump.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pygeom",
-    version="0.0.12",
+    version="0.0.13",
     author="Xero64",
     author_email="xero64@gmail.com",
     description="Geometry Definition and Calculation Package for Python",
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
-    python_requires='>=3.7'
+    python_requires='>=3.10'
 )
